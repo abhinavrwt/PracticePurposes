c68325919205e1f10d0ddbd565dbb49edeb652e3
abhinavrwt <37707653+abhinavrwt@users.noreply.github.com>
2018-10-02 13:51:04 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
-
+class Github (
+public : char x[20];
+void name()
+  {
+  cout <<"enter your name"<<endl;
+  cin>> x ;
+  }
+  )
+  ;
 int main() {
+  Github G1;
   cout << "Hello world";
+  G1.name();
   return 0;
 }
